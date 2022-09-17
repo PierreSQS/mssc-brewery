fbba3eb66957a8c653e706f56cc690cd11054c64
PierreSQS <nomo_messina@web.de>
2022-09-17 19:45:39 +0200

    Sec4_Chap29-Ass1-Create Get Endpoint: made the test passing

--- src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
+++ src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbrewery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,8 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class MsscBreweryApplicationTests {
 
+	@Autowired
+	ApplicationContext appCtx;
+
 	@Test
-	public void contextLoads(ApplicationContext appCtx) {
+	public void contextLoads() {
 		assertThat(appCtx).isNotNull();
 	}
 
