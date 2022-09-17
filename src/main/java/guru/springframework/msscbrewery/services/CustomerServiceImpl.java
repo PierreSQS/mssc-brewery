0849b24724ab7ed09ebd9e5f401bc0f8818f1c64
PierreSQS <nomo_messina@web.de>
2022-09-17 21:35:45 +0200

    Sec4_Chap29-Ass1-Create Get Endpoint: added the forgotten annotation on service

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerByID(UUID customerID) {
